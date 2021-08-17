2a4ab5ddc083db33061e8f451b5797a5e07f3355
Marlon Prado <marlonprado.cn@gmail.com>
2021-08-17 17:40:17 -0300

    Criando repository e service

---
+++ src/main/java/com/grupotema/portalcondo/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.grupotema.portalcondo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.grupotema.portalcondo.domain.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+}
--- src/main/java/com/grupotema/portalcondo/resources/UsuarioResource.java
+++ src/main/java/com/grupotema/portalcondo/resources/UsuarioResource.java
@@ -1,27 +1,25 @@
 package com.grupotema.portalcondo.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grupotema.portalcondo.domain.Usuario;
+import com.grupotema.portalcondo.services.UsuarioService;
 
 @RestController
-@RequestMapping(value="/Usuarios")
+@RequestMapping(value="/usuarios")
 public class UsuarioResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Usuario> listar() {
-		Usuario usr1 = new Usuario(1, "Maria dos Reis", "maria", "123");
-		Usuario usr2 = new Usuario(2, "Jose Pedro Dia", "josep", "321");
-		
-		List<Usuario> lista = new ArrayList<>();
-		lista.add(usr1);
-		lista.add(usr2);
-		
-		return lista;
+	@Autowired
+	private UsuarioService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Usuario obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
