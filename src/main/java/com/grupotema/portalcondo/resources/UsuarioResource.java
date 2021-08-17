a1c4f073acfd0b9664be86124c1b9102a08b214e
Marlon Prado <marlonprado.cn@gmail.com>
2021-08-17 14:46:31 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/grupotema/portalcondo/resources/UsuarioResource.java
+++ src/main/java/com/grupotema/portalcondo/resources/UsuarioResource.java
@@ -1,15 +1,27 @@
 package com.grupotema.portalcondo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.grupotema.portalcondo.domain.Usuario;
 
 @RestController
 @RequestMapping(value="/Usuarios")
 public class UsuarioResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando";
+	public List<Usuario> listar() {
+		Usuario usr1 = new Usuario(1, "Maria dos Reis", "maria", "123");
+		Usuario usr2 = new Usuario(2, "Jose Pedro Dia", "josep", "321");
+		
+		List<Usuario> lista = new ArrayList<>();
+		lista.add(usr1);
+		lista.add(usr2);
+		
+		return lista;
 	}
 }
