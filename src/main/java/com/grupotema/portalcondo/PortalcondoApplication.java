69879611595fcb2987f0e8f72566743d831d5cf2
Marlon Prado <marlonprado.cn@gmail.com>
2021-08-17 14:09:20 -0300

    Projeto criado

---
+++ src/main/java/com/grupotema/portalcondo/PortalcondoApplication.java
@@ -0,0 +1,13 @@
+package com.grupotema.portalcondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalcondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalcondoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupotema/portalcondo/PortalcondoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupotema.portalcondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalcondoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
